6dbed3fec5d083fba5a2cab9e1b5e24d91944797
Dayronaguilera <aguilera.dayron@gmail.com>
2021-06-11 12:21:30 +0200

    Refactoring 1

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ var output3 = document.getElementById("result3");
 
 // per creare dei numeri random
 function getRandomNumber(min, max) {
-    var result = Math.floor(Math.random() * max) + min;
-    return result;      
+
+    // farmi spiegare questo!
+    return Math.floor(Math.random() * (max - min + 1) + min);
+         
 }
 
 // array number inizialmente vuoto!
@@ -28,23 +30,27 @@ output.innerHTML ="I numeri del pc sono: " + listNumber + "</br>";
 
 //numero inserito dal utente con prompt!
 var listUserNumb = [];
+var score = 0;
 
-while (listUserNumb.length < 10) {
+while (listUserNumb.length < 5) {
 
     //chiedi al utente per 84 volte un numero!
     var userNumb = parseInt(prompt("inserisci un un numero da 1 a 100"));
     
-
-    // controla che il numero nn sia  presente nel array!
-    if (listNumber.includes(userNumb)) {
-        output3.innerHTML = "spiacente hai perso";
-        break;
-
-    } else if (!listUserNumb.includes(userNumb)) {
-          listUserNumb.push(userNumb)
-
-    } else {
-        alert("inserisci un'altro numero");
-    }   
+    // controla che il numero nn sia  presente nel array del pc!
+    if (!listUserNumb.includes(userNumb)) {
+        
+        if (listNumber.includes(userNumb)) {
+            output3.innerHTML = "Spiacente hai perso";
+            break;
+    
+        }else{listUserNumb.push(userNumb)
+            score += 1
+        }
+       
+    }else {
+        alert("inserisci un'altro numero");   
+    }
 }
-output2.innerHTML ="I numeri del utente sono: " + listUserNumb + "</br>";
+
+output2.innerHTML = "I numeri del utente sono: " + listUserNumb + "</br>" + "il tuo score è: " + score + "punti";
